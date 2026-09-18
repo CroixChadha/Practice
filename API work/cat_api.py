c31e6b69180e081a0737c312575f5549e9657002
CroixChadha <chadhakids2@gmail.com>
2026-09-17 19:05:11 -0500

    forgot to commit these changes

--- API work/cat_api.py
+++ API work/cat_api.py
@@ -6,15 +6,11 @@ import sys
 
 # changed the api to the cat facts api
 # take input to ask what the person wants to see.
-type_of_request = input("What would you like to see? Type 1 for cat facts. Type 2 for breeds. Type 3 to exit ")
+type_of_request = input("What would you like to see? Type 1 for a cat fact. Type 2 for breeds. Type 3 to exit ")
 # check for the the response type and append the param to the base url - OPTMIZE THIS
-if type_of_request == str(1):
-    param = "facts"
-elif type_of_request == str(2):
-    param = "breeds"
-else:
-    print("You selected Exit, bye!")
-    sys.exit()
+
+#request to the API for a fact
+def get_cat_fact():
+    cat_fact_request = requests.get('https://catfact.ninja/fact').json()
+    print(json.dumps(cat_fact_request, indent=2))
     
-request = requests.get('https://catfact.ninja/' + param).json()
-print(json.dumps(request, indent=2))

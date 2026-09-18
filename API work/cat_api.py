a0f6d29cdc314ca8f979311fb5e2c4d29cb96010
CroixChadha <chadhakids2@gmail.com>
2026-09-17 19:14:52 -0500

    moving this to another folder

--- API work/cat_api.py
+++ API work/cat_api.py
@@ -2,15 +2,17 @@ import requests
 import json
 import os
 import sys
+from flask import flask
 
 
 # changed the api to the cat facts api
-# take input to ask what the person wants to see.
-type_of_request = input("What would you like to see? Type 1 for a cat fact. Type 2 for breeds. Type 3 to exit ")
-# check for the the response type and append the param to the base url - OPTMIZE THIS
-
 #request to the API for a fact
 def get_cat_fact():
     cat_fact_request = requests.get('https://catfact.ninja/fact').json()
     print(json.dumps(cat_fact_request, indent=2))
-    
+
+def get_cat_breeds():
+    cat_breed_request = requests.get('https://catfact.ninja/breeds').json()
+    print(json.dumps(cat_breed_request, indent=2))
+
+choice = input("What would you like to choose?")

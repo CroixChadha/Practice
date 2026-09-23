7e8314a58c693c5dfb692f463bc6a81906e3aba5
CroixChadha <chadhakids2@gmail.com>
2026-09-22 21:06:40 -0500

    Feature is implemented: clicking the button for a single cat fact prints out a cat fact in the resultarea. This took unbelievably long to do for no reason. I am just stuck in python mode and I cant seem to lock in on js. The more I do this the more imposter syndrome I get. I believe that I know how to do this stuff but if i had to do this again from scratch I would probably not know how to do any of this. I guess this comes with being a programmer or i guess in the beginning at least. Well thats all for tonight.

--- Cat-API-Project/static/scripts/script.js
+++ Cat-API-Project/static/scripts/script.js
@@ -1,5 +1,43 @@
-const fact_button = async () => {
-    const response = await fetch ('https://catfact.ninja/fact');
-    const myJson = await response.json();
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
-fact_button.call()
+// function for the one cat fact button
+window.onload = function() {
+const fact = document.querySelector('.resultarea')
+const factbutton = document.querySelector('.fact')
+const getOneCatFact = async () => {
+    try{
+        const res = await fetch('https://catfact.ninja/fact')
+
+        const data = await res.json();
+        fact.textContent = data.fact
+    } catch(e) {
+        console.error(e)
+    }   
+    
+}
+factbutton.addEventListener('click', () => {
+    getOneCatFact()
+})
+
+
+//function for the facts one. 
+
+const facts = document.querySelector('.resultarea')
+const factsbutton = document.querySelector('.facts')
+const getCatFacts = async () => {
+    try{
+        const res = await fetch('https://catfact.ninja/facts?page=' + getRandomInt(34))
+
+        const data = await res.json();
+        facts.textContent = data.data
+        console.log(data)
+    } catch(e) {
+        console.error(e)
+    }   
+    
+}
+factsbutton.addEventListener('click', () => {
+    getCatFacts()
+})
+}
